3426e01d734a1d626b96c17b4366aa6558fd11a0
oriree70 <oriree70@gmail.com>
2024-07-10 02:41:54 +0000

    data source for ami

---
+++ jenks_server/data_source.tf
@@ -0,0 +1,23 @@
+# Get latest AMI ID for Amazon Linux2 OS
+
+data "aws_ami" "amzUbuntu" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+}
